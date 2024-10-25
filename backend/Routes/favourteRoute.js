becbfdbc3030b4ec2b0af13c46d3464df602c422
KeithPrakash <keith.prakash99@gmail.com>
2024-10-25 14:54:32 +0530

    final

--- backend/Routes/favourteRoute.js
+++ backend/Routes/favourteRoute.js
@@ -4,9 +4,9 @@ import { authMiddleware } from '../authMiddleware/authmiddleware.js';
 
 const router =express.Router()
 router.get('/list',authMiddleware,getFavorites);
-router.post('/add',authMiddleware,   addFavorite);
-router.delete('/:id',authMiddleware,   removeFavorite);
-router.get('/:id',authMiddleware,   removeFavorite);
+router.post('/add',authMiddleware, addFavorite);
+router.delete('/:id',authMiddleware,  removeFavorite);
+router.get('/:id',authMiddleware,  removeFavorite);
 
 
 export  default router;
